fca187ce316c17ff505fdb76674304843a7f3e37
bysoyeon <psy71731925@gmail.com>
2024-12-13 15:25:35 +0900

    fix: post/new 경로 오류 수정

--- frontend server/routes/board-route.js
+++ frontend server/routes/board-route.js
@@ -7,13 +7,13 @@ const BoardController = require("../controllers/board-controller");
 // 게시글 목록 페이지
 router.get("/post/list", BoardController.getList);
 
+// 게시글 신규 작성 페이지
+router.get("/post/new", BoardController.getNew);
+
 // 게시글 상세 페이지
-router.get("/post/:postId", BoardController.getDetail);
+router.get("/post/:postId(\\d+)", BoardController.getDetail);
 
 // 게시글 수정 페이지
-router.get("/post/:postId/update", BoardController.getModify);
-
-// 게시글 신규 작성 페이지
-router.get("/post/new", BoardController.getEdit);
+router.get("/post/update/:postId", BoardController.getModify);
 
 module.exports = router;
