14c809f6b166cb5d596234fee9df122e917b84a7
bysoyeon <psy71731925@gmail.com>
2024-12-31 20:38:41 +0900

    fix: 비밀번 호 수정 페이지 삭제 후 회원정보 변경 페이지로 통합

--- frontend server/controllers/auth-controller.js
+++ frontend server/controllers/auth-controller.js
@@ -6,13 +6,7 @@ exports.join = (req, res) => {
   res.sendFile(filePath);
 };
 
-// 비밀번호 변경 페이지 열기
-exports.password = (req, res) => {
-  const filePath = path.join(__dirname, "../../public/community-password.html");
-  res.sendFile(filePath);
-};
-
-// 닉네임 변경 페이지 열기
+// 회원정보 변경 페이지 열기
 exports.nickname = (req, res) => {
   const filePath = path.join(__dirname, "../../public/community-user.html");
   res.sendFile(filePath);
